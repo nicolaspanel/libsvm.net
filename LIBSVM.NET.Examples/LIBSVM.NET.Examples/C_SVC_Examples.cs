de31ebbaf575253d0ed73e79cbd7916ecc558c86
nicolaspanel <nicolas.panell@live.fr>
2014-01-31 18:21:26 +0100

    rename examples files

---
+++ LIBSVM.NET.Examples/LIBSVM.NET.Examples/C_SVC_Examples.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using libsvm;
+
+namespace LIBSVM.NET.Examples
+{
+    [TestClass()]
+    public class C_SVC_Examples
+    {
+        // Full leukemia dataset
+        private const string LEU_TEST_FILE = @"LIBSVM.NET.Examples\DataSets\leukemia\leu.ds.combined.txt";
+        
+        // Full svmguide1 dataset, see .\libsvm.net\LIBSVM.NET.Tests\DataSets\svmguide1 folder
+        //private const string SVMGUIDE1_TEST_FILE = "svmguide1.ds.combined.txt";
+        // Find more datasets in the libsvm official website : http://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/multiclass.html
+
+        static double C = 0.8;
+        static double gamma = 0.000030518125;
+
+        svm_problem _prob; 
+
+        [TestInitialize()]
+        public void TestInitialize()
+        {
+            var path = Environment.CurrentDirectory;
+            var pos = path.IndexOf("libsvm.net");
+            var basePath = path.Substring(0, pos + 10);
+            string fullPath = System.IO.Path.Combine(basePath, LEU_TEST_FILE);
+
+            // get data from file
+            // Note that you should always scale your data
+            _prob = ProblemHelper.ReadAndScaleProblem(fullPath);
+        }
+
+
+        /// <summary>
+        /// Show how to get the accuracy using cross validation method
+        /// Assert accurancy is greater than zero
+        ///</summary>
+        [TestMethod()]
+        public void DoCrossValidationTest()
+        {
+            var svm = new C_SVC(_prob, KernelHelper.RadialBasisFunctionKernel(gamma), C);
+            var cva = svm.GetCrossValidationAccuracy(5); 
+
+            Assert.IsTrue(cva > 0);
+        }
+
+        /// <summary>
+        ///Show how to predict probabilities for classification problems
+        ///Verify that the prediction is always the most probable class
+        ///</summary>
+        [TestMethod()]
+        public void PredictTest()
+        {
+            var svm = new C_SVC(_prob, KernelHelper.RadialBasisFunctionKernel(gamma), C);
+            var nb_class = _prob.y.Distinct().Count();
+            for (int i = 0; i < _prob.l; i++)
+            {
+                var x = _prob.x[i];
+                var y = _prob.y[i];
+                var probabilities = svm.PredictProbabilities(x);
+                var predict = svm.Predict(x);
+                Assert.IsTrue(predict == probabilities.OrderByDescending(p => p.Value).First().Key);
+                Assert.IsNotNull(probabilities);
+                Assert.IsTrue(probabilities.Count == nb_class);
+                var sum = probabilities.Sum(e => e.Value) ;
+                
+            }
+        }
+    }
+}
---
+++ LIBSVM.NET.Examples/LIBSVM.NET.Examples/ProblemHelper_Examples.cs
@@ -0,0 +1,118 @@
+﻿using libsvm;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace LIBSVM.NET.Examples
+{
+    
+    
+    /// <summary>
+    ///Classe de test pour ProblemHelperTests, destinée à contenir tous
+    ///les tests unitaires ProblemHelperTests
+    ///</summary>
+    [TestClass()]
+    public class ProblemHelper_Examples
+    {
+        private const string TEST_FILE  = @"LIBSVM.NET.Examples\DataSets\leukemia\leu.ds.combined.txt";
+        private const string WRITE_FILE = @"LIBSVM.NET.Examples\DataSets\leukemia\leu.ds.writed.txt";
+        private string base_path = "";
+        
+        [TestInitialize()]
+        public void TestInitialize()
+        {
+            var current_path = Environment.CurrentDirectory;
+            var pos = current_path.IndexOf("libsvm.net");
+            base_path = current_path.Substring(0, pos + 10);
+        }
+        
+        /// <summary>
+        ///Test pour ReadAndScaleProblem
+        ///</summary>
+        [TestMethod()]
+        public void ReadAndScaleProblemTest()
+        {
+            string full_path = System.IO.Path.Combine(base_path, TEST_FILE);
+            var prob = ProblemHelper.ReadAndScaleProblem(full_path);
+            Assert.IsNotNull(prob);
+            Assert.IsTrue(prob.x.Max(v => v.Max(n => n.value)) == 1.0);
+            Assert.IsTrue(prob.x.Min(v => v.Min(n => n.value)) == -1.0);
+        }
+
+        /// <summary>
+        ///Test pour ReadProblem
+        ///</summary>
+        [TestMethod()]
+        public void ReadProblemTest()
+        {
+            string full_path = System.IO.Path.Combine(base_path, TEST_FILE);
+            var prob = ProblemHelper.ReadProblem(full_path);
+            Assert.IsNotNull(prob);
+        }
+
+        /// <summary>
+        ///Test pour ScaleProblem
+        ///</summary>
+        [TestMethod()]
+        public void ScaleProblemTest()
+        {
+            string full_path = System.IO.Path.Combine(base_path, TEST_FILE);
+            var prob = ProblemHelper.ScaleProblem(ProblemHelper.ReadProblem(full_path));
+            Assert.IsNotNull(prob);
+            Assert.IsTrue(prob.x.Max(v => v.Max(n => n.value)) == 1.0);
+            Assert.IsTrue(prob.x.Min(v => v.Min(n => n.value)) == -1.0);
+        }
+
+        /// <summary>
+        ///Test pour WriteProblem
+        ///</summary>
+        [TestMethod()]
+        public void WriteProblemTest()
+        {
+            string full_test_path = System.IO.Path.Combine(base_path, TEST_FILE);
+            var prob = ProblemHelper.ReadProblem(full_test_path);
+
+            string full_write_path = System.IO.Path.Combine(base_path, WRITE_FILE);
+            ProblemHelper.WriteProblem(full_write_path, prob);
+        }
+
+        /// <summary>
+        ///Test pour atof
+        ///</summary>
+        [TestMethod()]
+        public void atofTest()
+        {
+            string s = "1.0"; 
+            double expected = 1.0; 
+            double actual;
+            actual = ProblemHelper.atof(s);
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        ///Test pour atof
+        ///</summary>
+        [TestMethod()]
+        public void atofTest2()
+        {
+            string s = "1";
+            double expected = 1.0;
+            double actual;
+            actual = ProblemHelper.atof(s);
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        ///Test pour atoi
+        ///</summary>
+        [TestMethod()]
+        public void atoiTest()
+        {
+            string s = "2"; // TODO: initialisez à une valeur appropriée
+            int expected = 2; // TODO: initialisez à une valeur appropriée
+            int actual;
+            actual = ProblemHelper.atoi(s);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
